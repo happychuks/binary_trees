97d4500b5493a8ef469240d3003c01926f050004
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-02-29 01:45:28 -0800

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,10 @@ typedef struct levelorder_queue_s
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
 
+void binary_tree_print(const binary_tree_t *);
 
-#endif
+/* Mandatory Tasks */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+#endif /* Prepared by Happy F. Chukwuma */
